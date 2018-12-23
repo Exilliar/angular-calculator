0dc67114119217654fdaeee8f0eb991f8ef341bb
Exilliar <rorywebber1000@gmail.com>
2018-12-23 23:31:30 +0000

    made the calculation appear on the input field

--- calculator/src/app/pages/calculator/calculator.component.ts
+++ calculator/src/app/pages/calculator/calculator.component.ts
@@ -15,12 +15,28 @@ export class CalculatorComponent implements OnInit {
   clickNumber(number)// sends the number that was clicked to the server
   {
     console.log(number);
+    this.addToCalc(number);
     return number;
   }
   clickSymbol(symbol)// sends the symbl that was clicked to the server
   {
     console.log(symbol);
+    this.addToCalc(symbol);
     return symbol;
   }
 
+  addToCalc(toAdd)
+  {
+    let calculation = (<HTMLInputElement>document.getElementById("outputResult")).value;
+    if(toAdd != "=")
+    {
+      calculation += toAdd;
+    }
+    else
+    {
+      calculation = "";
+    }
+    (<HTMLInputElement>document.getElementById("outputResult")).value = calculation;
+    console.log(calculation);
+  }
 }
